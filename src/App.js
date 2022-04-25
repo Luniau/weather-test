e0466011f4fbd59cf47a9bc5967e1805c45b1cba
Luniau <lubecarvalho@hotmail.com>
2022-04-25 18:47:28 +0200

    Created the new app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-     Weather app
+      <div className="container">
+
+        <Weather />
+
+        <footer>
+          This project was coded by <a href="https://precious-otter-6afb15.netlify.app/" target="_blank" rel="noreferrer">Luciana Carvalho</a>  and is
+          <a href="https://github.com/Luniau/Weather-React" target="_blank" rel="noreferrer"> open-sourced on github </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
