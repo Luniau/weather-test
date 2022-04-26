5b8f24d948311986b7ab748b7a3e05364ffc1f14
Luniau <lubecarvalho@hotmail.com>
2022-04-26 15:29:01 +0200

    changed web address

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 
         <footer>
           This project was coded by <a href="https://precious-otter-6afb15.netlify.app/" target="_blank" rel="noreferrer">Luciana Carvalho</a>  and is
-          <a href="https://github.com/Luniau/Weather-React" target="_blank" rel="noreferrer"> open-sourced on github </a>
+          <a href="https://github.com/Luniau/weather-test" target="_blank" rel="noreferrer"> open-sourced on github </a>
         </footer>
       </div>
     </div>
